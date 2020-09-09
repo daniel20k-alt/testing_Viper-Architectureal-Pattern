7d39df0a43f9593fa1027cbb8b98ec80ee150c89
Testing_author <testing_beta@mail.com>
2020-09-09 08:56:56 +0300

    Initial Commit

---
+++ testing_Viper Architectureal Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing_Viper Architectureal Pattern
+//
+//  Created by DDDD on 09/09/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
